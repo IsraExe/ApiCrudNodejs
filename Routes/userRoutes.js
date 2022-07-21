ed0b7eaa74df95c38a06c1a9f9ca988e087ab9bf
IsraExe <israel-w@hotmail.com>
2022-07-21 10:48:50 -0300

    Add update user  and adjust findUser

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,24 +6,25 @@ const router = express.Router()
 
 router.get('/getall', async (req, res) => {
     try {
-        const user = await Users.find({})
-        if (!user) return res.status(500).send({ err: 'Erro na consulta de usuários'})
-        return res.status(200).send(user)
+        const AllUsers = await Users.find({})
+        if (!AllUsers) return res.status(500).send({ err: 'Erro na consulta de usuários'})
+        return res.status(200).send(AllUsers)
     } catch (error) {
         return res.status(500).send(error)
     }
 })
-
-// you can find a user by username or data 
+ 
 router.get('/finduser', async (req, res) => {
-    const {username, created} = req.query 
+    const {username} = req.query 
 
-    if (!username && !created) return res.status(400).send({err: 'Username or created required!'})
+    if (!username ) return res.status(400).send({err: 'Username or created required!'})
     
     try {
-        const user = Users.find({username}) 
-        if (!user) return res.status(400).send({err: "Usuário não encontrado!"}) 
-        return res.status(200).send(user)
+        const schemaUsersTree = Users.find({}).schema.tree
+        console.log(Object.keys(schemaUsersTree))
+        const userFound = await Users.findOne({username}) 
+        if (!userFound) return res.status(400).send({err: "Usuário não encontrado!"}) 
+        return res.status(200).send(userFound)
     } catch (error) {
         return res.status(500).send(error)
     }
@@ -36,13 +37,41 @@ router.post('/create', async (req, res) => {
     if (!username || !email) return res.status(400).send({msg: 'Username and email required!'})
 
     try {
-        const user = await Users.find({username}) || await Users.findOne({email})
-        if (user) return res.send({msg: 'Usuário já registrado'})
-    
+        const checkUserExists = await Users.find({username}) || await Users.findOne({email})
+        if (checkUserExists) return res.send({msg: 'Usuário já registrado'})
+        
         const createdUser = await Users.create(query)
         return res.status(202).send(createdUser)
     } catch (error) {
         return res.status(500).send(error)
+    }
+})
+
+router.patch('/update/:username', async (req, res) => {
+    const fields = ['username', 'email'];
+
+    const username = req.params.username;
+    const query = req.query;
+    const params = Object.keys(query);
+
+    if (params.length === 0) return res.status(400).send({ message: 'Empty query' });
+
+    for (let param of params) {
+        if (!fields.includes(param)) return res.status(406).send({ message: 'Algum campo inserido não existe ou não pode ser alterado!' });
+    }
+
+    try {
+        const user = await Users.findOne({ username });
+
+        if (user) {
+            const updatedUser = await Users.findByIdAndUpdate(user.id, query, { new: true });
+            
+            return res.status(202).send(updatedUser);
+        } else {
+            return res.status(406).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        return res.status(400).send({ message: err.message });
     }
 })
 
@@ -53,8 +82,8 @@ router.delete('/delete', async (req, res) => {
 
     try {
         if (!username && !email) return res.status(400).send({msg: 'Username or email required'})
-        const user = await Users.findOneAndDelete({username}) || await Users.findOneAndDelete({email}) 
-        if (!user) return res.status(400).send({msg: `${capitalize} não encontrado!`})
+        const checkUser = await Users.findOneAndDelete({username}) || await Users.findOneAndDelete({email}) 
+        if (!checkUser) return res.status(400).send({msg: `${capitalize} não encontrado!`})
         return res.status(410).send({msg: 'Usuário deletado'})  
     } catch (error) {
         return res.status(500).send(error)  
